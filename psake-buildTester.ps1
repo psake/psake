c1980115515c7cde2a603f2ec4204b36090f6384
Jorge Matos <matos_jorge@hotmail.com>
2009-05-04 05:40:06 +0000

    Created new version of psake that runs as a module and Run-Psake is now an advanced function.
    I also changed the psake-buildTester.ps1 to use the new Run-Psake function.

--- psake-buildTester.ps1
+++ psake-buildTester.ps1
@@ -1,40 +1,43 @@
-powershell -nologo -noprofile -output "Text" -command {
-	function runBuilds{
-		$buildFiles = dir examples\*.ps1
-		$testResults = @()	
-		
-		foreach($buildFile in $buildFiles) {					
-			$testResult = "" | select Name, Result 
-			$testResult.Name = $buildFile.Name
-			.\psake.ps1 $buildFile | Out-Null			
-			$testResult.Result = (getResult $buildFile.Name $?)
-			$testResults += $testResult 			
-		}
-		return $testResults
-	}
+function runBuilds{
+	$buildFiles = dir examples\*.ps1
+	$testResults = @()	
 	
-	function getResult([string]$fileName, [bool]$buildSucceeded) {		
-		if ($fileName.StartsWith("bad")) {
-			if (!$buildSucceeded) {
-				"Passed"
-			} 
-			else {
-				"Failed"
-			}	
-		}
+	foreach($buildFile in $buildFiles) {					
+		$testResult = "" | select Name, Result 
+		$testResult.Name = $buildFile.Name
+		Run-psake $buildFile -noexit | Out-Null			
+		$testResult.Result = (getResult $buildFile.Name $global:psake_buildSucceeded)
+		$testResults += $testResult 			
+	}
+	return $testResults
+}
+
+function getResult([string]$fileName, [bool]$buildSucceeded) {		
+	if ($fileName.StartsWith("bad")) {
+		if (!$buildSucceeded) {
+			"Passed"
+		} 
 		else {
-			if ($buildSucceeded) {
-				"Passed"
-			} 
-			else {
-				"Failed"
-			}
+			"Failed"
+		}	
+	}
+	else {
+		if ($buildSucceeded) {
+			"Passed"
+		} 
+		else {
+			"Failed"
 		}
 	}
-
-	$results = runBuilds
-	$results | ft -auto
-		
-	$failed = $results | ? { $_.Result -eq "Failed" }
-	if ($failed) {exit 1} else {exit 0}	
 }
+
+$results = runBuilds
+$results | ft -auto
+	
+$failed = $results | ? { $_.Result -eq "Failed" }
+if ($failed) {
+	"One of the builds failed"
+	exit 1
+} else {
+	exit 0
+}	
--- psake.psm1
+++ psake.psm1
@@ -18,45 +18,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-param(
-  [string]$buildFile = 'default.ps1',
-  [string[]]$taskList = @(),
-  [string]$framework = '3.5',
-  [switch]$debug = $false,
-  [switch]$help  = $false,
-  [switch]$timing = $false,
-  [switch]$docs = $false
-)
-
-if($help) {
-@"
-psake [buildFile] [tasks] [-framework ver] [-debug] [-timing] [-docs]
-  where buildFile is the name of the build file, (default: default.ps1)
-        tasks is a list of tasks to execute from the build file,
-        ver is the .NET Framework version to target - 1.0, 1.1, 2.0, 3.0, or 3.5 (default)
-        debug dumps information on the properties, includes, and tasks, as well as more detailed error information.
-        timing prints a report showing how long each task took to execute
-        docs prints a list of available tasks
-
-psake -help
-  Displays this message.
-"@
-  return
-}
-
-$global:tasks = @{}
-$global:properties = @()
-$global:includes = New-Object System.Collections.Queue
-$global:psake_version = "0.23"
-$global:psake_buildScript = $buildFile
-$global:psake_frameworkVersion = $framework
-
-$script:executedTasks = New-Object System.Collections.Stack
-$script:callStack = New-Object System.Collections.Stack
-$script:originalEnvPath = $env:path
-$script:originalDirectory = Get-Location
-$originalErrorActionPreference = $Global:ErrorActionPreference
-
 function task([string]$name=$null, [scriptblock]$action = $null, [scriptblock]$precondition = $null, [scriptblock]$postcondition = $null, [switch]$continueOnError = $false, [string[]]$depends = @(), [string]$description = $null) {
   if (($name -eq $null) -or ($name.Trim() -eq "")) {
 	  throw "Error: Task must have a name"	
@@ -184,10 +145,15 @@ function Cleanup-Environment {
   $env:path = $script:originalEnvPath	
   Set-Location $script:originalDirectory
   $global:ErrorActionPreference = $originalErrorActionPreference
-  remove-variable tasks -scope "global" 
-  remove-variable properties -scope "global"
-  remove-variable includes -scope "global"
-  remove-variable psake_* -scope "global"  
+  remove-variable tasks -scope "global" -ErrorAction SilentlyContinue
+  remove-variable properties -scope "global" -ErrorAction SilentlyContinue
+  remove-variable includes -scope "global" -ErrorAction SilentlyContinue
+  remove-variable psake_version -scope "global" -ErrorAction SilentlyContinue 
+  remove-variable psake_buildScript -scope "global" -ErrorAction SilentlyContinue 
+  remove-variable psake_frameworkVersion -scope "global" -ErrorAction SilentlyContinue 
+  if (!$noexit) {
+	remove-variable psake_buildSucceeded -scope "global" -ErrorAction SilentlyContinue 
+  }  
 }
 
 #borrowed from Jeffrey Snover http://blogs.msdn.com/powershell/archive/2006/12/07/resolve-error.aspx
@@ -224,71 +190,9 @@ function exec([string]$command, [string]$parameters) {
     }
 }
 
-function Run-Psake {
-  trap {
-    Cleanup-Environment
-    Write-Host -foregroundcolor Red $_
-    if($debug) {
-      "-" * 80
-      "An Error Occurred. See Error Details Below:"
-      "-" * 80
-      Resolve-Error
-      "-" * 80
-    }
-    exit(1)
-  }
-
-  $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-
-  # Execute the build file to set up the tasks and defaults
-  if(test-path $buildFile) {
-    $buildFile = resolve-path $buildFile
-    set-location (split-path $buildFile)
-    & $buildFile
-  } else {
-    throw "Error: Could not find the build file, $buildFile."
-  }
-
-  if($debug) {
-    Dump-Includes
-    Dump-Properties
-    Dump-Tasks
-  }
-
-  Configure-BuildEnvironment
-
-  # N.B. The initial dot (.) indicates that variables initialized/modified
-  #      in the propertyBlock are available in the parent scope.
-  while ($global:includes.Count -gt 0) {
-  	$includeBlock = $global:includes.Dequeue();
-  	. $includeBlock;
-  }
-  foreach($propertyBlock in $global:properties) {
-    . $propertyBlock
-  }
-
-  if($docs) {
-    Write-Documentation
-    Cleanup-Environment
-    exit(0)
-  }
-  
-  # Execute the list of tasks or the default task
-  if($taskList.Length -ne 0) {
-    foreach($task in $taskList) {
-      ExecuteTask $task
-    }
-  } elseif ($global:tasks.default -ne $null) {
-    ExecuteTask default
-  } else {
-    throw 'Error: default task required'
-  }
-
-  $stopwatch.Stop()
-
-  if ($timing) {	
+function PrintBuildTimingReport() {
 	"-"*70
-    "Build Time Report"
+	"Build Time Report"
 	"-"*70	
 	$list = @()
 	while ($script:executedTasks.Count -gt 0) {
@@ -302,10 +206,177 @@ function Run-Psake {
 	[Array]::Reverse($list)
 	$list += "" | Select-Object @{Name="Name";Expression={"Total:"}}, @{Name="Duration";Expression={$stopwatch.Elapsed}}
 	$list | Format-Table -Auto | Out-String -Stream | ? {$_}  # using "Out-String -Stream" to filter out the blank line that Format-Table prepends 
-  }
-
-  # Clear out any global variables
-  Cleanup-Environment
 }
 
-Run-Psake
+function Run-psake {
+<#
+.Synopsis
+    Runs a psake build script.
+.Description
+    This function runs a psake build script 
+.Parameter BuildFile 
+	The psake build script to execute (default: default.ps1).	
+.Parameter Framework 
+	The version of the .NET framework you want to build
+	Possible values: '1.0', '1.1', '2.0', '3.0',  '3.5'
+	Default = '3.5'
+.Parameter debuginfo 
+	Dumps information on the properties, includes, and tasks, as well as more detailed error information.
+.Parameter timing 
+	Prints a report showing how long each task took to execute
+.Parameter docs 
+	Prints a list of tasks and their descriptions
+.Parameter noexit
+	Does not use 'exit' command when an error occurs or when printing documentation - useful when running a build interactively
+	
+.Example
+    Run-psake 
+	
+	Runs the 'default' task in the 'default.ps1' build script in the current directory
+.Example
+	Run-psake '.\build.ps1'
+	
+	Runs the 'default' task in the '.build.ps1' build script
+.Example
+	Run-psake '.\build.ps1' Tests, Package
+	
+	Runs the 'Tests' and 'Package' tasks in the '.build.ps1' build script
+.Example
+	Run-psake '.\build.ps1' -timing
+	
+	Runs the 'default' task in the '.build.ps1' build script and prints a timing report
+.Example
+	Run-psake '.\build.ps1' -debuginfo
+	
+	Runs the 'default' task in the '.build.ps1' build script and prints a report of what includes, properties and tasks are in the build script
+.Example
+	Run-psake '.\build.ps1' -docs
+	
+	Prints a report of all the tasks and their descriptions and exits
+	
+.ReturnValue
+    Calls exit() function with 0 for success and 1 for failure 
+	If $noexit is $true then exit() is not called and no value is returned  
+.Link	
+.Notes
+ NAME:      Run-psake
+ AUTHOR:    Jorge Matos
+ LASTEDIT:  05/04/2009
+#Requires -Version 2.0
+#>
+[CmdletBinding(
+    SupportsShouldProcess=$False,
+    SupportsTransactions=$False, 
+    ConfirmImpact="None",
+    DefaultParameterSetName="")]
+	
+	param(
+	  [string]$buildFile = 'default.ps1',
+	  [string[]]$taskList = @(),
+	  [string]$framework = '3.5',
+	  [switch]$debuginfo = $false,	 
+	  [switch]$timing = $false,
+	  [switch]$docs = $false,
+	  [switch]$noexit = $false
+	)
+
+	Begin {
+		$global:tasks = @{}
+		$global:properties = @()
+		$global:includes = New-Object System.Collections.Queue
+		$global:psake_version = "0.23"
+		$global:psake_buildScript = $buildFile
+		$global:psake_frameworkVersion = $framework
+		$global:psake_buildSucceeded = $true
+
+		$script:executedTasks = New-Object System.Collections.Stack
+		$script:callStack = New-Object System.Collections.Stack
+		$script:originalEnvPath = $env:path
+		$script:originalDirectory = Get-Location
+		$originalErrorActionPreference = $Global:ErrorActionPreference
+	}
+	
+	Process {	
+		try {
+			$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+
+			# Execute the build file to set up the tasks and defaults
+			if(test-path $buildFile) {
+				$buildFile = resolve-path $buildFile
+				set-location (split-path $buildFile)
+				& $buildFile
+			} else {
+				throw "Error: Could not find the build file, $buildFile."
+			}
+
+			if($debuginfo) {
+				Dump-Includes
+				Dump-Properties
+				Dump-Tasks
+			}
+
+			Configure-BuildEnvironment
+
+			# N.B. The initial dot (.) indicates that variables initialized/modified
+			#      in the propertyBlock are available in the parent scope.
+			while ($global:includes.Count -gt 0) {
+			$includeBlock = $global:includes.Dequeue()
+				. $includeBlock;
+			}
+			foreach($propertyBlock in $global:properties) {
+				. $propertyBlock
+			}
+
+			if($docs) {
+				Write-Documentation
+				Cleanup-Environment
+				if ($noexit) { 					
+					return
+				} else {
+					exit(0)
+				}				
+			}
+
+			# Execute the list of tasks or the default task
+			if($taskList.Length -ne 0) {
+				foreach($task in $taskList) {
+				  ExecuteTask $task
+				}
+			} elseif ($global:tasks.default -ne $null) {
+				ExecuteTask default
+			} else {
+				throw 'Error: default task required'
+			}
+
+			$stopwatch.Stop()
+
+			if ($timing) {	
+				PrintBuildTimingReport
+			}
+			
+			$global:psake_buildSucceeded = $true
+		} catch {
+			Cleanup-Environment
+			Write-Host -foregroundcolor Red $_
+			if($debuginfo) {
+			  "-" * 80
+			  "An Error Occurred. See Error Details Below:"
+			  "-" * 80
+			  Resolve-Error
+			  "-" * 80
+			}
+			if ($noexit) { 
+				$global:psake_buildSucceeded = $false				 
+			} else {
+				exit(1)
+			}
+		}
+	} #Process
+	
+	End {
+		# Clear out any global variables
+		Cleanup-Environment
+	}
+}
+
+Export-ModuleMember "Run-psake", "task", "properties", "include"
