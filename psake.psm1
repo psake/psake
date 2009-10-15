3522f4bf888062f4775836af535b299020449c53
James Kovacs <jkovacs@post.harvard.edu>
2009-10-14 22:00:27 -0600

    Updated the version number to v2.01 for release.

--- psake.psm1
+++ psake.psm1
@@ -1,5 +1,5 @@
-# psake v2.00
-# Copyright � 2009 James Kovacs
+# psake v2.01
+# Copyright (C) 2009 James Kovacs
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
