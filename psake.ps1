8be9e1875c570b4c1400e28e4bbbfec4b74a0bb8
jmatos <matos.jorge@gmail.com>
2010-04-22 23:34:57 -0500

    from lanwin's fork - allows you to load the psake.psm1 module without having to cd into the directory containing the psake.psm1 file

--- psake.ps1
+++ psake.ps1
@@ -1,5 +1,4 @@
-# Helper script for those who want to run
-# psake without importing the module.
+# Helper script for those who want to run psake without importing the module.
 $scriptPath = Split-Path -parent $MyInvocation.InvocationName;
 import-module (join-path $scriptPath psake.psm1)
 invoke-psake @args
