d28b7d5bd549e12d10e3c66cf4ca074062be8103
jmatos <matos.jorge@gmail.com>
2010-04-23 01:06:20 -0500

    Added try/finally to ensure that module is removed if there is an error - added example in comments

--- psake.ps1
+++ psake.ps1
@@ -1,5 +1,14 @@
 # Helper script for those who want to run psake without importing the module.
-$scriptPath = Split-Path -parent $MyInvocation.InvocationName;
-import-module (join-path $scriptPath psake.psm1)
-invoke-psake @args
-remove-module psake
+# Example:
+# .\psake.ps1 "default.ps1" "BuildHelloWord" "4.0" 
+#
+try
+{
+	$scriptPath = Split-Path -parent $MyInvocation.InvocationName;
+	import-module (join-path $scriptPath psake.psm1)
+	invoke-psake @args
+}
+finally
+{
+	remove-module psake
+}
