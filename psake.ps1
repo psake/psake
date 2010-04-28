64ea235ff890f013427b93551681ad94f3d0c7f1
James Kovacs <jkovacs@post.harvard.edu>
2010-04-27 21:15:16 -0600

    Fixed issue with psake.ps1 failing when passing named parameters.

--- psake.ps1
+++ psake.ps1
@@ -1,14 +1,28 @@
 # Helper script for those who want to run psake without importing the module.
 # Example:
 # .\psake.ps1 "default.ps1" "BuildHelloWord" "4.0" 
-#
-try
-{
-	$scriptPath = Split-Path -parent $MyInvocation.InvocationName;
-	import-module (join-path $scriptPath psake.psm1)
-	invoke-psake @args
-}
-finally
-{
-	remove-module psake -ea "SilentlyContinue"
+
+# Must match parameter definitions for psake.psm1/invoke-psake 
+# otherwise named parameter binding fails
+param(
+  [Parameter(Position=0,Mandatory=0)]
+  [string]$buildFile = 'default.ps1',
+  [Parameter(Position=1,Mandatory=0)]
+  [string[]]$taskList = @(),
+  [Parameter(Position=2,Mandatory=0)]
+  [string]$framework = '3.5',
+  [Parameter(Position=3,Mandatory=0)]
+  [switch]$docs = $false,
+  [Parameter(Position=4,Mandatory=0)]
+  [System.Collections.Hashtable]$parameters = @{},
+  [Parameter(Position=5, Mandatory=0)]
+  [System.Collections.Hashtable]$properties = @{}
+)
+
+try {
+  $scriptPath = Split-Path -parent $MyInvocation.InvocationName;
+  import-module (join-path $scriptPath psake.psm1)
+  invoke-psake $buildFile $taskList $framework $docs $parameters $properties
+} finally {
+  remove-module psake -ea "SilentlyContinue"
 }
