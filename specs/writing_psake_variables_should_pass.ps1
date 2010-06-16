51d6106f0533b02aaa79fe6d75064911ab0e61ee
jmatos <matos.jorge@gmail.com>
2010-06-15 23:40:58 -0500

    1) Updated psake-buildTester.ps1 so that a message is displayed while the test builds are running 2) Removed reference to "log_error" from writing_psake_variables_should_pass.ps1 test build

--- specs/writing_psake_variables_should_pass.ps1
+++ specs/writing_psake_variables_should_pass.ps1
@@ -14,14 +14,12 @@ task Verify -description "This task verifies psake's variables" {
   
   Assert ($psake.ContainsKey("build_success")) "psake variable does not contain 'build_success'"
   Assert ($psake.ContainsKey("use_exit_on_error")) "psake variable does not contain 'use_exit_on_error'"
-  Assert ($psake.ContainsKey("log_error")) "psake variable does not contain 'log_error'"
   Assert ($psake.ContainsKey("version")) "psake variable does not contain 'version'"
   Assert ($psake.ContainsKey("build_script_file")) "psake variable does not contain 'build_script_file'"
   Assert ($psake.ContainsKey("framework_version")) "psake variable does not contain 'framework_version'"
   
   Assert (!$psake.build_success) 'psake.build_success should be $false'
   Assert (!$psake.use_exit_on_error) 'psake.use_exit_on_error should be $false'
-  Assert (!$psake.log_error) 'psake.log_error should be $false'
   Assert (![string]::IsNullOrEmpty($psake.version)) 'psake.version was null or empty'
   Assert ($psake.build_script_file -ne $null) '$psake.build_script_file was null' 
   Assert ($psake.build_script_file.Name -eq "writing_psake_variables_should_pass.ps1") ("psake variable: {0} was not equal to 'writing_psake_variables_should_pass.ps1'" -f $psake.build_script_file.Name)
