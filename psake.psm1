50693a174ded4b7b94870f59f59ad47ece7966c2
Jorge Matos <matos.jorge@gmail.com>
2011-02-27 19:19:57 -0600

    Moved the xml comment helps from the source code into help file.  This will make it easier to modify the code in the future.

--- psake.psm1
+++ psake.psm1
@@ -20,6 +20,8 @@
 
 #Requires -Version 2.0
 
+remove-module psake -erroraction silentlycontinue
+
 DATA msgs
 {
 convertfrom-stringdata @'
@@ -54,14 +56,12 @@ import-localizeddata -bindingvariable msgs -erroraction silentlycontinue
 #-- Private Module Functions
 function Load-Configuration
 {
-	$psake.config = new-object psobject -property @{
-	  defaultbuildfilename="default.ps1";
-	  tasknameformat="Executing {0}";
-	  exitcode="1";
-	  modules=(new-object psobject -property @{ autoload=$false })
-	}
-		
-	$psakeConfigFilePath = (join-path $PSScriptRoot psake-config.ps1)
+	param
+	(
+		[string] $configdir = $PSScriptRoot
+	)
+	
+	$psakeConfigFilePath = (join-path $configdir psake-config.ps1)
 	
 	if (test-path $psakeConfigFilePath)
 	{
@@ -72,6 +72,18 @@ function Load-Configuration
 		catch
 		{
 			throw "Error Loading Configuration from psake-config.ps1: " + $_
+		}
+	}
+	else
+	{
+		if (!$psake.config)
+		{
+			$psake.config = new-object psobject -property @{
+				defaultbuildfilename="default.ps1";
+				tasknameformat="Executing {0}";
+				exitcode="1";
+				modules=(new-object psobject -property @{ autoload=$false })
+			}
 		}
 	}
 }
@@ -234,35 +246,13 @@ function Write-TaskTimeSummary
 }
 
 #-- Public Module Functions
+
+# .ExternalHelp  psake.psm1-help.xml
 function Invoke-Task
 {
-<#
-.SYNOPSIS
-This function allows you to call a target from another target
-
-.DESCRIPTION
-This is a function that will allow you to invoke a function from within another function
-
-.PARAMETER taskName
-The name of the task to execute
-
-.EXAMPLE
-invoke-task clean
-
-This example calls "clean" task
-
-.LINK
-Assert
-Invoke-psake
-Task
-Properties
-Include
-FormatTaskName
-TaskSetup
-TaskTearDown
-#>
 	[CmdletBinding()]
-	param([Parameter(Position=0,Mandatory=1)][string]$taskName)
+	param([Parameter(Position=0,Mandatory=1)]
+	[string]$taskName)
 	
 	Assert $taskName ($msgs.error_invalid_task_name)
 
@@ -378,41 +368,10 @@ TaskTearDown
 	$executedTasks.Push($taskKey)
 }
 
+# .ExternalHelp  psake.psm1-help.xml
 function Exec
 {
-<#
-.SYNOPSIS
-Helper function for executing command-line programs.
-
-.DESCRIPTION
-This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode to see if an error occcured.
-If an error is detected then an exception is thrown.  This function allows you to run command-line programs without
-having to explicitly check fthe $lastexitcode variable.
-
-.PARAMETER cmd
-The scriptblock to execute.  This scriptblock will typically contain the command-line invocation.
-Required
-
-.PARAMETER errorMessage
-The error message used for the exception that is thrown.
-Optional
-
-.EXAMPLE
-exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
-
-This example calls the svn command-line client.
-
-.LINK
-Assert
-Invoke-psake
-Task
-Properties
-Include
-FormatTaskName
-TaskSetup
-TaskTearDown
-#>
-  [CmdletBinding()]
+    [CmdletBinding()]
 
 	param(
 		[Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
@@ -425,60 +384,12 @@ TaskTearDown
 	}
 }
 
+# .ExternalHelp  psake.psm1-help.xml
 function Assert
 {
-<#
-.SYNOPSIS
-Helper function for "Design by Contract" assertion checking.
-
-.DESCRIPTION
-This is a helper function that makes the code less noisy by eliminating many of the "if" statements
-that are normally required to verify assumptions in the code.
-
-.PARAMETER conditionToCheck
-The boolean condition to evaluate
-Required
-
-.PARAMETER failureMessage
-The error message used for the exception if the conditionToCheck parameter is false
-Required
-
-.EXAMPLE
-Assert $false "This always throws an exception"
-
-This example always throws an exception
-
-.EXAMPLE
-Assert ( ($i % 2) -eq 0 ) "%i is not an even number"
-
-This exmaple may throw an exception if $i is not an even number
-
-.LINK
-Invoke-psake
-Task
-Properties
-Include
-FormatTaskName
-TaskSetup
-TaskTearDown
-
-.NOTES
-It might be necessary to wrap the condition with paranthesis to force PS to evaluate the condition
-so that a boolean value is calculated and passed into the 'conditionToCheck' parameter.
-
-Example:
-    Assert 1 -eq 2 "1 doesn't equal 2"
-
-PS will pass 1 into the condtionToCheck variable and PS will look for a parameter called "eq" and
-throw an exception with the following message "A parameter cannot be found that matches parameter name 'eq'"
-
-The solution is to wrap the condition in () so that PS will evaluate it first.
-
-    Assert (1 -eq 2) "1 doesn't equal 2"
-#>
   [CmdletBinding()]
-
-	param(
+   
+  param(
 		[Parameter(Position=0,Mandatory=1)]$conditionToCheck,
 		[Parameter(Position=1,Mandatory=1)]$failureMessage
 	)
@@ -488,114 +399,9 @@ The solution is to wrap the condition in () so that PS will evaluate it first.
 	}
 }
 
+# .ExternalHelp  psake.psm1-help.xml
 function Task
 {
-<#
-.SYNOPSIS
-Defines a build task to be executed by psake
-
-.DESCRIPTION
-This function creates a 'task' object that will be used by the psake engine to execute a build task.
-Note: There must be at least one task called 'default' in the build script
-
-.PARAMETER Name
-The name of the task
-Required
-
-.PARAMETER Action
-A scriptblock containing the statements to execute
-Optional
-
-.PARAMETER PreAction
-A scriptblock to be executed before the 'Action' scriptblock.
-Note: This parameter is ignored if the 'Action' scriptblock is not defined.
-Optional
-
-.PARAMETER PostAction
-A scriptblock to be executed after the 'Action' scriptblock.
-Note: This parameter is ignored if the 'Action' scriptblock is not defined.
-Optional
-
-.PARAMETER Precondition
-A scriptblock that is executed to determine if the task is executed or skipped.
-This scriptblock should return $true or $false
-Optional
-
-.PARAMETER Postcondition
-A scriptblock that is executed to determine if the task completed its job correctly.
-An exception is thrown if the scriptblock returns $false.
-Optional
-
-.PARAMETER ContinueOnError
-If this switch parameter is set then the task will not cause the build to fail when an exception is thrown
-
-.PARAMETER Depends
-An array of tasks that this task depends on.  They will be executed before the current task is executed.
-
-.PARAMETER Description
-A description of the task.
-
-.EXAMPLE
-A sample build script is shown below:
-
-task default -depends Test
-
-task Test -depends Compile, Clean {
-  "This is a test"
-}
-
-task Compile -depends Clean {
-  "Compile"
-}
-
-task Clean {
-  "Clean"
-}
-
-The 'default' task is required and should not contain an 'Action' parameter.
-It uses the 'depends' parameter to specify that 'Test' is a dependency
-
-The 'Test' task uses the 'depends' parameter to specify that 'Compile' and 'Clean' are dependencies
-The 'Compile' task depends on the 'Clean' task.
-
-Note:
-The 'Action' parameter is defaulted to the script block following the 'Clean' task.
-
-The equivalent 'Test' task is shown below:
-
-task Test -depends Compile, Clean -Action {
-  $testMessage
-}
-
-The output for the above sample build script is shown below:
-Executing task, Clean...
-Clean
-Executing task, Compile...
-Compile
-Executing task, Test...
-This is a test
-
-Build Succeeded!
-
-----------------------------------------------------------------------
-Build Time Report
-----------------------------------------------------------------------
-Name    Duration
-----    --------
-Clean   00:00:00.0065614
-Compile 00:00:00.0133268
-Test    00:00:00.0225964
-Total:  00:00:00.0782496
-
-.LINK
-Invoke-psake
-Properties
-Include
-FormatTaskName
-TaskSetup
-TaskTearDown
-Assert
-#>
   [CmdletBinding()]
   
   param(
@@ -654,447 +460,50 @@ Assert
 	$currentContext.tasks.$taskKey = $newTask
 }
 
+# .ExternalHelp  psake.psm1-help.xml
 function Properties
 {
-<#
-.SYNOPSIS
-Define a scriptblock that contains assignments to variables that will be available to all tasks in the build script
-
-.DESCRIPTION
-A build script may declare a "Properies" function which allows you to define
-variables that will be available to all the "Task" functions in the build script.
-
-.PARAMETER properties
-The script block containing all the variable assignment statements
-Required
-
-.EXAMPLE
-A sample build script is shown below:
-
-Properties {
-  $build_dir = "c:\build"
-  $connection_string = "datasource=localhost;initial catalog=northwind;integrated security=sspi"
-}
-
-Task default -depends Test
-
-Task Test -depends Compile, Clean {
-}
-
-Task Compile -depends Clean {
-}
-
-Task Clean {
-}
-
-.LINK
-Invoke-psake
-Task
-Include
-FormatTaskName
-TaskSetup
-TaskTearDown
-Assert
-
-.NOTES
-You can have more than 1 "Properties" function defined in the script
-#>
 	[CmdletBinding()]
 	param([Parameter(Position=0,Mandatory=1)][scriptblock]$properties)	
 	$psake.context.Peek().properties += $properties
 }
 
+# .ExternalHelp  psake.psm1-help.xml
 function Include
 {
-<#
-.SYNOPSIS
-Include the functions or code of another powershell script file into the current build script's scope
-
-.DESCRIPTION
-A build script may declare an "includes" function which allows you to define
-a file containing powershell code to be included and added to the scope of
-the currently running build script.
-
-.PARAMETER fileNamePathToInclude
-A string containing the path and name of the powershell file to include
-Required
-
-.EXAMPLE
-A sample build script is shown below:
-
-Include ".\build_utils.ps1"
-
-Task default -depends Test
-
-Task Test -depends Compile, Clean {
-}
-
-Task Compile -depends Clean {
-}
-
-Task Clean {
-}
-
-
-.LINK
-Invoke-psake
-Task
-Properties
-FormatTaskName
-TaskSetup
-TaskTearDown
-Assert
-
-.NOTES
-You can have more than 1 "Include" function defined in the script
-#>
 	[CmdletBinding()]
 	param([Parameter(Position=0,Mandatory=1)][string]$fileNamePathToInclude)
 	Assert (test-path $fileNamePathToInclude) ($msgs.error_invalid_include_path -f $fileNamePathToInclude)
 	$psake.context.Peek().includes.Enqueue((Resolve-Path $fileNamePathToInclude));
 }
 
+# .ExternalHelp  psake.psm1-help.xml
 function FormatTaskName
 {
-<#
-.SYNOPSIS
-Allows you to define a format mask that will be used when psake displays
-the task name
-
-.DESCRIPTION
-Allows you to define a format mask that will be used when psake displays
-the task name.  The default is "Executing task, {0}..."
-
-.PARAMETER format
-A string containing the format mask to use, it should contain a placeholder ({0})
-that will be used to substitute the task name.
-Required
-
-.EXAMPLE
-A sample build script is shown below:
-
-FormatTaskName "[Task: {0}]"
-
-Task default -depends Test
-
-Task Test -depends Compile, Clean {
-}
-
-Task Compile -depends Clean {
-}
-
-Task Clean {
-}
-
-You should get the following output:
-------------------------------------
-
-[Task: Clean]
-[Task: Compile]
-[Task: Test]
-
-Build Succeeded
-
-----------------------------------------------------------------------
-Build Time Report
-----------------------------------------------------------------------
-Name    Duration
-----    --------
-Clean   00:00:00.0043477
-Compile 00:00:00.0102130
-Test    00:00:00.0182858
-Total:  00:00:00.0698071
-
-.LINK
-Invoke-psake
-Include
-Task
-Properties
-TaskSetup
-TaskTearDown
-Assert
-#>
 	[CmdletBinding()]
 	param([Parameter(Position=0,Mandatory=1)]$format)
 	$psake.context.Peek().formatTaskName = $format
 }
 
+# .ExternalHelp  psake.psm1-help.xml
 function TaskSetup
 {
-<#
-.SYNOPSIS
-Adds a scriptblock that will be executed before each task
-
-.DESCRIPTION
-This function will accept a scriptblock that will be executed before each
-task in the build script.
-
-.PARAMETER include
-A scriptblock to execute
-Required
-
-.EXAMPLE
-A sample build script is shown below:
-
-Task default -depends Test
-
-Task Test -depends Compile, Clean {
-}
-
-Task Compile -depends Clean {
-}
-
-Task Clean {
-}
-
-TaskSetup {
-  "Running 'TaskSetup' for task $context.Peek().currentTaskName"
-}
-
-You should get the following output:
-------------------------------------
-
-Running 'TaskSetup' for task Clean
-Executing task, Clean...
-Running 'TaskSetup' for task Compile
-Executing task, Compile...
-Running 'TaskSetup' for task Test
-Executing task, Test...
-
-Build Succeeded
-
-----------------------------------------------------------------------
-Build Time Report
-----------------------------------------------------------------------
-Name    Duration
-----    --------
-Clean   00:00:00.0054018
-Compile 00:00:00.0123085
-Test    00:00:00.0236915
-Total:  00:00:00.0739437
-
-.LINK
-Invoke-psake
-Include
-Task
-Properties
-FormatTaskName
-TaskTearDown
-Assert
-#>
 	[CmdletBinding()]
 	param([Parameter(Position=0,Mandatory=1)][scriptblock]$setup)
 	$psake.context.Peek().taskSetupScriptBlock = $setup
 }
 
+# .ExternalHelp  psake.psm1-help.xml
 function TaskTearDown
 {
-<#
-.SYNOPSIS
-Adds a scriptblock that will be executed after each task
-
-.DESCRIPTION
-This function will accept a scriptblock that will be executed after each
-task in the build script.
-
-.PARAMETER include
-A scriptblock to execute
-Required
-
-.EXAMPLE
-A sample build script is shown below:
-
-Task default -depends Test
-
-Task Test -depends Compile, Clean {
-}
-
-Task Compile -depends Clean {
-}
-
-Task Clean {
-}
-
-TaskTearDown {
-  "Running 'TaskTearDown' for task $context.Peek().currentTaskName"
-}
-
-You should get the following output:
-------------------------------------
-
-Executing task, Clean...
-Running 'TaskTearDown' for task Clean
-Executing task, Compile...
-Running 'TaskTearDown' for task Compile
-Executing task, Test...
-Running 'TaskTearDown' for task Test
-
-Build Succeeded
-
-----------------------------------------------------------------------
-Build Time Report
-----------------------------------------------------------------------
-Name    Duration
-----    --------
-Clean   00:00:00.0064555
-Compile 00:00:00.0218902
-Test    00:00:00.0309151
-Total:  00:00:00.0858301
-
-.LINK
-Invoke-psake
-Include
-Task
-Properties
-FormatTaskName
-TaskSetup
-Assert
-#>
 	[CmdletBinding()]
 	param([Parameter(Position=0,Mandatory=1)][scriptblock]$teardown)
 	$psake.context.Peek().taskTearDownScriptBlock = $teardown
 }
 
+# .ExternalHelp  psake.psm1-help.xml
 function Invoke-psake
 {
-<#
-.SYNOPSIS
-Runs a psake build script.
-
-.DESCRIPTION
-This function runs a psake build script
-
-.PARAMETER BuildFile
-The psake build script to execute (default: default.ps1).
-
-.PARAMETER TaskList
-A comma-separated list of task names to execute
-
-.PARAMETER Framework
-The version of the .NET framework you want to build. You can append x86 or x64 to force a specific framework. If not specified, x86 or x64 will be detected based on the bitness of the PowerShell process.
-Possible values: '1.0', '1.1', '2.0', '2.0x86', '2.0x64', '3.0', '3.0x86', '3.0x64', '3.5', '3.5x86', '3.5x64', '4.0', '4.0x86', '4.0x64'
-Default = '3.5'
-
-.PARAMETER Docs
-Prints a list of tasks and their descriptions
-
-.PARAMETER Parameters
-A hashtable containing parameters to be passed into the current build script.  These parameters will be processed before the 'Properties' function of the script is processed.  This means you can access parameters from within the 'Properties' function!
-
-.PARAMETER Properties
-A hashtable containing properties to be passed into the current build script.  These properties will override matching properties that are found in the 'Properties' function of the script.
-
-.EXAMPLE
-Invoke-psake
-
-Runs the 'default' task in the 'default.ps1' build script in the current directory
-
-.EXAMPLE
-Invoke-psake '.\build.ps1'
-
-Runs the 'default' task in the '.build.ps1' build script
-
-.EXAMPLE
-Invoke-psake '.\build.ps1' Tests,Package
-
-Runs the 'Tests' and 'Package' tasks in the '.build.ps1' build script
-
-.EXAMPLE
-Invoke-psake Tests
-
-If you have your Tasks in the .\default.ps1. This example will run the 'Tests' tasks in the 'default.ps1' build script.
-
-.EXAMPLE
-Invoke-psake 'Tests, Package'
-
-If you have your Tasks in the .\default.ps1. This example will run the 'Tests' and 'Package' tasks in the 'default.ps1' build script.
-NOTE: the quotes around the list of tasks to execute.
-
-.EXAMPLE
-Invoke-psake '.\build.ps1' -docs
-
-Prints a report of all the tasks and their descriptions and exits
-
-.EXAMPLE
-Invoke-psake .\parameters.ps1 -parameters @{"p1"="v1";"p2"="v2"}
-
-Runs the build script called 'parameters.ps1' and passes in parameters 'p1' and 'p2' with values 'v1' and 'v2'
-
-.EXAMPLE
-Invoke-psake .\properties.ps1 -properties @{"x"="1";"y"="2"}
-
-Runs the build script called 'properties.ps1' and passes in parameters 'x' and 'y' with values '1' and '2'
-
-.OUTPUTS
-  If there is an exception and the build script was invoked by a windows service (directly/indirectly)
-  then runs exit(1) to set the DOS lastexitcode variable
-  otherwise set the '$psake.build_success variable' to $true or $false depending
-  on whether an exception was thrown
-
-.NOTES
-When the psake module is loaded a variabled called $psake is created it is a hashtable
-containing some variables that can be used to configure psake:
-
-$psake.build_success = $false       # indicates that the current build was successful
-$psake.version = "4.00"             # contains the current version of psake
-$psake.build_script_file = $null    # contains a System.IO.FileInfo for the current build file
-$psake.build_script_dir				# contains the fully qualified path to the current build file
-$psake.framework_version = ""       # contains the framework version # for the current build
-
-You should see the following when you display the contents of the $psake variable right after importing psake
-
-PS projects:\psake> Import-Module .\psake.psm1
-PS projects:\psake> $psake
-
-Name                           Value
-----                           -----
-version                        4.00
-build_script_file
-build_script_dir
-build_success                  False
-framework_version
-
-After a build is executed the following $psake values are updated (build_script_file, build_script_dir, build_success, and framework_version)
-
-PS projects:\psake> Invoke-psake .\examples\default.ps1
-Executing task: Clean
-Executed Clean!
-Executing task: Compile
-Executed Compile!
-Executing task: Test
-Executed Test!
-
-Build Succeeded!
-
-----------------------------------------------------------------------
-Build Time Report
-----------------------------------------------------------------------
-Name    Duration
-----    --------
-Clean   00:00:00.0798486
-Compile 00:00:00.0869948
-Test    00:00:00.0958225
-Total:  00:00:00.2712414
-
-PS projects:\psake> $psake
-
-Name                           Value
-----                           -----
-version                        4.00
-build_script_file              C:\Users\Jorge\Documents\Projects\psake\examples\default.ps1
-build_script_dir			   C:\Users\Jorge\Documents\Projects\psake\examples	
-build_success                  True
-framework_version              3.5
-
-.LINK
-Task
-Include
-Properties
-FormatTaskName
-TaskSetup
-TaskTearDown
-Assert
-#>
 	[CmdletBinding()]
 	param(
 		[Parameter(Position=0,Mandatory=0)]
@@ -1113,6 +522,7 @@ Assert
 
 	try
 	{
+		"psake version {0}`nCopyright (c) 2010 James Kovacs`n" -f $psake.version
 		$psake.build_success = $false
 		$psake.framework_version = $framework
 
@@ -1177,6 +587,9 @@ Assert
 
 		$psake.build_script_file = Get-Item $buildFile
 		$psake.build_script_dir = $psake.build_script_file.DirectoryName
+		
+		Load-Configuration $psake.build_script_dir
+		
 		set-location $psake.build_script_dir
 		. $psake.build_script_file.FullName
 
