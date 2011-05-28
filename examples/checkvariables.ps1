7b004f1dcce42b7c538e4b59a6f32869f2d75522
Rafal Klys <rafalklys@wp.pl>
2011-05-28 15:53:15 +0200

    Simplified checkvariables.ps1 example
    Removed usage of foreach and variable: prefix

--- examples/checkvariables.ps1
+++ examples/checkvariables.ps1
@@ -1,34 +1,26 @@
 ﻿Properties {
-	$x = 1
-	$y = 2
+  $x = 1
+  $y = 2
 }
 
 FormatTaskName "[{0}]"
 
 Task default -Depends Verify 
 
-Task Verify -Description "This task verifies psake's variables" {	
-	
-	$assertions = @( 
-		((Test-Path 'variable:\psake'), "'psake' variable was not exported from module"),
-		(($variable:psake.ContainsKey("build_success")), "psake variable does not contain 'build_success'"),
-		(($variable:psake.ContainsKey("version")), "psake variable does not contain 'version'"),
-		(($variable:psake.ContainsKey("build_script_file")), "psake variable does not contain 'build_script_file'"),
-		(($variable:psake.ContainsKey("framework_version")), "psake variable does not contain 'framework_version'"),		
-		((!$variable:psake.build_success), 'psake.build_success should be $false'),
-		((![string]::IsNullOrEmpty($variable:psake.version)), 'psake.version was null or empty'),
-		(($variable:psake.build_script_file -ne $null), '$psake.build_script_file was null'), 
-		(($variable:psake.build_script_file.Name -eq "checkvariables.ps1"), ("psake variable: {0} was not equal to 'checkvariables.ps1'" -f $psake.build_script_file.Name)),
-		((![string]::IsNullOrEmpty($variable:psake.framework_version)), 'psake variable: $psake.framework_version was null or empty'),
-		(($variable:psake.context.Peek().tasks.Count -ne 0), 'psake variable: $tasks had length zero'),
-		(($variable:psake.context.Peek().properties.Count -ne 0), 'psake variable: $properties had length zero'),
-		(($variable:psake.context.Peek().includes.Count -eq 0), 'psake variable: $includes should have had length zero'),
-		(($variable:psake.context.Peek().formatTaskNameString -ne ""), 'psake variable: $formatTaskNameString was not set correctly'),
-		(($variable:psake.context.Peek().currentTaskName -eq "Verify"), 'psake variable: $currentTaskName was not set correctly')		
-	)
-	
-	foreach ($assertion in $assertions)
-	{	
-		Assert ( $assertion[0] ) $assertion[1]		
-	}	
+Task Verify -Description "This task verifies psake's variables" {
+  Assert (Test-Path 'variable:\psake') "'psake' variable was not exported from module"
+  Assert ($psake.ContainsKey("build_success")) "psake variable does not contain 'build_success'"
+  Assert ($psake.ContainsKey("version")) "psake variable does not contain 'version'"
+  Assert ($psake.ContainsKey("build_script_file")) "psake variable does not contain 'build_script_file'"
+  Assert ($psake.ContainsKey("framework_version")) "psake variable does not contain 'framework_version'"
+  Assert (!$psake.build_success) 'psake.build_success should be $false'
+  Assert ($psake.version) 'psake.version was null or empty'
+  Assert ($psake.build_script_file) '$psake.build_script_file was null'
+  Assert ($psake.build_script_file.Name -eq "checkvariables.ps1") ("psake variable: {0} was not equal to 'checkvariables.ps1'" -f $psake.build_script_file.Name)
+  Assert ($psake.framework_version) 'psake variable: $psake.framework_version was null or empty'
+  Assert ($psake.context.Peek().tasks.Count -ne 0) 'psake variable: $tasks had length zero'
+  Assert ($psake.context.Peek().properties.Count -ne 0) 'psake variable: $properties had length zero'
+  Assert ($psake.context.Peek().includes.Count -eq 0) 'psake variable: $includes should have had length zero'
+  Assert ($psake.context.Peek().formatTaskNameString -ne "") 'psake variable: $formatTaskNameString was not set correctly'
+  Assert ($psake.context.Peek().currentTaskName -eq "Verify") 'psake variable: $currentTaskName was not set correctly'
 }
