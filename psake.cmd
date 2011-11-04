e17c6dddc044d66938759ebd59e45c11966d85e4
whut <rafal.klys@wp.pl>
2011-11-04 22:40:39 +0100

    Fix for issue #23 - psake.cmd should exit with 0 when build succeeded
    Also use of -ExecutionPolicy Bypass, which is even less restrictive than -ExecutionPolicy Unrestricted

--- psake.cmd
+++ psake.cmd
@@ -8,8 +8,8 @@ if '%1'=='?' goto usage
 if '%1'=='/help' goto usage
 if '%1'=='help' goto usage
 
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "& '%DIR%psake.ps1' %*; if ($psake.build_success -eq $false) { exit 1 }"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%psake.ps1' %*; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
 
 goto :eof
 :usage
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "& '%DIR%psake-help.ps1'"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%psake-help.ps1'"
