53bb4f7874f2b1062d7754ea4259c5336ce4005a
Damian Hickey <dhickey@gmail.com>
2012-06-13 22:31:17 +0100

    Fixed package script to exclude nuget directory and nuget.ext. Bumped package version to 4.2.0.1

--- NuGetPackageBuilder.ps1
+++ NuGetPackageBuilder.ps1
@@ -7,8 +7,7 @@ if(Test-Path $destDir -PathType container){
 	Remove-Item $destDir -Recurse -Force
 }
 
-robocopy $dir "$destDir\tools" /E /B /NP /R:0 /W:0 /NJH /NJS /NS /NFL /NDL /XF ".git*" "Nuget*" "*.nupkg"  /XD "%DIR%nuget" "$dir.git" "$destDir"
-robocopy $dir "$destDir\tools" /E /B /NP /R:0 /W:0 /NJH /NJS /NS /NFL /NDL /XF ".git*" "Nuget*" "*.nupkg"  /XD "%DIR%nuget" "$dir.git" "$destDir"
+robocopy $dir "$destDir\tools" /E /B /NP /R:0 /W:0 /NJH /NJS /NS /NFL /NDL /XF ".git*" "Nuget*" "*.nupkg"  /XD "$dir\nuget" "$dir\.git" "$destDir" "$dir\nuget.exe"
 robocopy "$dir\nuget" $destDir /E /B /NP /R:0 /W:0 /NJH /NJS /NS /NFL /NDL
 
 .\nuget pack "$destDir\psake.nuspec"
