8ee664b87c0bb59378c09b10781d2b2ae86391e7
James Kovacs <jkovacs@post.harvard.edu>
2012-10-09 09:35:23 -0600

    Extracted license.txt and updated copyright date

--- psake.psm1
+++ psake.psm1
@@ -1,5 +1,5 @@
 # psake
-# Copyright (c) 2010 James Kovacs
+# Copyright (c) 2012 James Kovacs
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
