285fe822bcda290bad220eee5843309a64aebfab
Eric J. Smith <eric@codesmithtools.com>
2013-12-02 15:40:37 -0600

    Fix reversed logic and update test to include an assert.

--- specs/dotNet4.5.1_should_pass.ps1
+++ specs/dotNet4.5.1_should_pass.ps1
@@ -1,7 +1,8 @@
-$framework = '4.5.1'
+Framework "4.5.1"
 
 task default -depends MsBuild
 
 task MsBuild {
-  exec { msbuild /version }
+  $output = &msbuild /version 2>&1
+  Assert ($output -NotLike "12.0") '$output should contain 12.0'
 }
