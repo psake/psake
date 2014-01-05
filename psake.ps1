psake/psake

commit e49692ad3df1fd89411b8f6d3fcd705685efa4e5

New fix for issue #87
This fixes issue introduced long time ago in d706e5290a1195fd3647e135cc21a4e9172f45a3
The problem was when psake was imported as a module in one dir, and then executed without arguments from another
Also added using current config, not config_default when detecting build file.
Also use the same default parameters in psake.ps1 as in Invoke-Psake

psake.ps1
@@ -6,7 +6,7 @@
 # otherwise named parameter binding fails
 param(
     [Parameter(Position=0,Mandatory=0)]
-    [string]$buildFile = 'default.ps1',
+    [string]$buildFile,
     [Parameter(Position=1,Mandatory=0)]
     [string[]]$taskList = @(),
     [Parameter(Position=2,Mandatory=0)]

psake.psm1
@@ -318,9 +318,12 @@ function Invoke-psake {
             "psake version {0}`nCopyright (c) 2010 James Kovacs`n" -f $psake.version
         }
 
-        # If the default.ps1 file exists and the given "buildfile" isn 't found assume that the given
-        # $buildFile is actually the target Tasks to execute in the default.ps1 script.
-        if ($buildFile -and !(test-path $buildFile -pathType Leaf) -and (test-path $psake.config_default.buildFileName -pathType Leaf)) {
+        if (!$buildFile) {
+          $buildFile = $psake.config_default.buildFileName
+        }
+        elseif (!(test-path $buildFile -pathType Leaf) -and (test-path $psake.config_default.buildFileName -pathType Leaf)) {
+            # If the $config.buildFileName file exists and the given "buildfile" isn 't found assume that the given
+            # $buildFile is actually the target Tasks to execute in the $config.buildFileName script.
             $taskList = $buildFile.Split(', ')
             $buildFile = $psake.config_default.buildFileName
         }
@@ -442,9 +445,9 @@ function Invoke-psake {
         if ( $inNestedScope ) {
             throw $_
         } else {
-	        if (!$psake.run_by_psake_build_tester) {
-	            WriteColoredOutput $error_message -foregroundcolor Red
-	        }
+            if (!$psake.run_by_psake_build_tester) {
+                WriteColoredOutput $error_message -foregroundcolor Red
+            }
         }
     } finally {
         CleanupEnvironment

commit 8ce44c0955d6d62953eab4196e636d20d5be0c21

Dropped changing CUrrentCulture to prevent "turkish i" problem (issue #45) as it is now correctly fixed in c35bb4c5f8cbafb8ee535dbf996dfa2c231bd5bb (issue #70)
Also psake.ps1 should not change it's environment in any way, it's rude:)
Also fix for crash introduced in previous commit (e49692ad3df1fd89411b8f6d3fcd705685efa4e5) when removing default value for $buildFile argument

psake.ps1
@@ -28,11 +28,6 @@ param(
     [string]$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.path)
 )
 
-$currentThread = [System.Threading.Thread]::CurrentThread
-$invariantCulture = [System.Globalization.CultureInfo]::InvariantCulture
-$currentThread.CurrentCulture = $invariantCulture
-$currentThread.CurrentUICulture = $invariantCulture
-
 # '[p]sake' is the same as 'psake' but $Error is not polluted
 remove-module [p]sake
 import-module (join-path $scriptPath psake.psm1)
@@ -41,7 +36,7 @@ if ($help) {
   return
 }
 
-if (-not(test-path $buildFile)) {
+if ($buildPath -and (-not(test-path $buildFile))) {
     $absoluteBuildFile = (join-path $scriptPath $buildFile)
     if (test-path $absoluteBuildFile) {
         $buildFile = $absoluteBuildFile
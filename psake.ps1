8ce44c0955d6d62953eab4196e636d20d5be0c21
whut <rafalklys@wp.pl>
2014-01-06 00:20:04 +0100

    Dropped changing CUrrentCulture to prevent "turkish i" problem (issue #45) as it is now correctly fixed in c35bb4c5f8cbafb8ee535dbf996dfa2c231bd5bb (issue #70)
    Also psake.ps1 should not change it's environment in any way, it's rude:)
    Also fix for crash introduced in previous commit (e49692ad3df1fd89411b8f6d3fcd705685efa4e5) when removing default value for $buildFile argument

--- psake.ps1
+++ psake.ps1
@@ -28,11 +28,6 @@ param(
     [string]$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.path)
 )
 
-$currentThread = [System.Threading.Thread]::CurrentThread
-$invariantCulture = [System.Globalization.CultureInfo]::InvariantCulture
-$currentThread.CurrentCulture = $invariantCulture
-$currentThread.CurrentUICulture = $invariantCulture
-
 # '[p]sake' is the same as 'psake' but $Error is not polluted
 remove-module [p]sake
 import-module (join-path $scriptPath psake.psm1)
@@ -41,7 +36,7 @@ if ($help) {
   return
 }
 
-if (-not(test-path $buildFile)) {
+if ($buildPath -and (-not(test-path $buildFile))) {
     $absoluteBuildFile = (join-path $scriptPath $buildFile)
     if (test-path $absoluteBuildFile) {
         $buildFile = $absoluteBuildFile
