e5b7252bfdc8ca9697aeb93359969f9d395341bb
Damian Powell <damian@shadow-angel.com>
2016-09-06 13:18:48 +0100

    Fixed broken `Framework` specs.

--- specs/dotNet4.5.1_should_pass.ps1
+++ specs/dotNet4.5.1_should_pass.ps1
@@ -1,11 +1,11 @@
 Framework '4.5.1x86'
 
-task default -depends MsBuild
+Task default -depends MsBuild
 
-task MsBuild {
-    if ( $IsMacOS -OR $IsLinux ) {}
-    else {
-        $output = &msbuild /version /nologo 2>&1
-        Assert ($output -NotLike '12.0') '$output should contain 12.0'
-    }
+Task MsBuild {
+  if ( $IsMacOS -OR $IsLinux ) {}
+  else {
+    $output = &msbuild /version /nologo 2>&1
+    Assert ($output -match "\s12.0\b") '$output should contain 12.0'
+  }
 }
