fd81467c09c2b9714a5318dee40979adb0b13a6e
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-11-07 20:08:00 -0800

    Update build tester to cope with a single test file
    Previously if there was only one test file the build tester would fail as it was
    not an array type.  This commit detects if the return type is not an array and
    if so, forces the return type by a simple array literal.

--- psake-buildTester.ps1
+++ psake-buildTester.ps1
@@ -27,6 +27,10 @@ function Main()
 function runBuilds()
 {
 	$buildFiles = ls specs\*.ps1
+	if ($buildFiles.GetType().ToString() -ne 'System.Object[]') {
+		# If only one build file is found, force the return type to an array
+		$buildFiles = @($buildFiles)
+  }
 	$testResults = @()
 
 	#Add a fake build file to the $buildFiles array so that we can verify
