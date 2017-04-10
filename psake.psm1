82a3127d3adc9e516b12d59bb904a88f67a0a674
Paul Milbank <paulm@outlook.co.nz>
2017-04-10 19:51:44 +1200

    initial change to allow .net 4.6.2 detection

--- psake.psm1
+++ psake.psm1
@@ -576,7 +576,7 @@ function ConfigureBuildEnvironment {
             $versions = @('v4.0.30319')
             $buildToolsVersions = @('14.0', '12.0')
         }
-        {($_ -eq '4.6') -or ($_ -eq '4.6.1')} {
+        {($_ -ge '4.6')} {
             $versions = @('v4.0.30319')
             $buildToolsVersions = @('14.0')
         }
