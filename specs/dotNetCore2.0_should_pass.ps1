a0e38d10d0925af8cab164dd0421c8814a84205e
Mark SchillI <Mark.Schill@cmschill.net>
2017-07-11 13:33:30 -0400

    Fixes dotnet Core tests

--- specs/dotNetCore2.0_should_pass.ps1
+++ specs/dotNetCore2.0_should_pass.ps1
@@ -4,7 +4,7 @@ task default -depends MsBuild
 
 task MsBuild {
     if ( $IsOSX -OR $IsLinux ) {}
-    $output = &dotnet -version -nologo 2>&1
+    $output = &dotnet build -version -nologo 2>&1
     Assert ($output -NotLike "15.3") '$output should contain 15.3'
 }
 
