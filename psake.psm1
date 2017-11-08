ed7437c7d1b63b3b92058b8a1e6956f7d4b8bc31
Bevan Arps <bevan.arps@outlook.com>
2017-11-08 17:42:28 +1300

    (GH218) Improve Build Time Report
    * Use taskNameFormat to generate build time report header at end of build
    * Show build times to millisecond accuracy, no higher

--- psake.psm1
+++ psake.psm1
@@ -842,11 +842,18 @@ function WriteDocumentation($showDetailed) {
 
 function WriteTaskTimeSummary($invokePsakeDuration) {
     if ($psake.context.count -gt 0) {
-        "-" * 70
-        "Build Time Report"
-        "-" * 70
-        $list = @()
         $currentContext = $psake.context.Peek()
+        if ($currentContext.config.taskNameFormat -is [ScriptBlock]) {
+            & $currentContext.config.taskNameFormat "Build Time Report"
+        } elseif ($currentContext.config.taskNameFormat -ne "Executing {0}") {
+            $currentContext.config.taskNameFormat -f "Build Time Report"
+        }
+        else {  
+            "-" * 70
+            "Build Time Report"
+            "-" * 70
+        }
+        $list = @()
         while ($currentContext.executedTasks.Count -gt 0) {
             $taskKey = $currentContext.executedTasks.Pop()
             $task = $currentContext.tasks.$taskKey
@@ -855,13 +862,13 @@ function WriteTaskTimeSummary($invokePsakeDuration) {
             }
             $list += new-object PSObject -property @{
                 Name = $task.Name;
-                Duration = $task.Duration
+                Duration = $task.Duration.ToString("hh\:mm\:ss\.fff")
             }
         }
         [Array]::Reverse($list)
         $list += new-object PSObject -property @{
             Name = "Total:";
-            Duration = $invokePsakeDuration
+            Duration = $invokePsakeDuration.ToString("hh\:mm\:ss\.fff")
         }
         # using "out-string | where-object" to filter out the blank line that format-table prepends
         $list | format-table -autoSize -property Name,Duration | out-string -stream | where-object { $_ }
