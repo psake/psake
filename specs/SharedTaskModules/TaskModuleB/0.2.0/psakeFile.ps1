07c833552d5ddf0c219206ca0f85b8ed0ba5e44f
Brandon Olin <brandon@devblackops.io>
2018-03-09 22:01:09 -0800

    Add shared task module

---
+++ specs/SharedTaskModules/TaskModuleB/0.2.0/psakeFile.ps1
@@ -0,0 +1,8 @@
+
+task TaskAFromModuleB {
+    'Executing [TaskA] from module [TaskModuleB] version [0.2.0]'
+}
+
+task TaskBFromModuleB {
+    'Executing [TaskB] from module [TaskModuleB] version [0.2.0]'
+}
