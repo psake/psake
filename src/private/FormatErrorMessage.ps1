bff94cd159181f312eae9320e5efaa634591d55b
Brandon Olin <brandon@devblackops.io>
2019-04-05 11:38:46 -0700

    Fix reference to error record

--- src/private/FormatErrorMessage.ps1
+++ src/private/FormatErrorMessage.ps1
@@ -10,16 +10,16 @@ function FormatErrorMessage
     if ($currentConfig.verboseError) {
         $error_message = "{0}: An Error Occurred. See Error Details Below: $($script:nl)" -f (Get-Date)
         $error_message += ("-" * 70) + $script:nl
-        $error_message += "Error: {0}$($script:nl)" -f (ResolveError $_ -Short)
+        $error_message += "Error: {0}$($script:nl)" -f (ResolveError $ErrorRecord -Short)
         $error_message += ("-" * 70) + $script:nl
-        $error_message += ResolveError $_
+        $error_message += ResolveError $ErrorRecord
         $error_message += ("-" * 70) + $script:nl
         $error_message += "Script Variables" + $script:nl
         $error_message += ("-" * 70) + $script:nl
         $error_message += get-variable -scope script | format-table | out-string
     } else {
         # ($_ | Out-String) gets error messages with source information included.
-        $error_message = "Error: {0}: $($script:nl){1}" -f (Get-Date), (ResolveError $_ -Short)
+        $error_message = "Error: {0}: $($script:nl){1}" -f (Get-Date), (ResolveError $ErrorRecord -Short)
 }
 
     $error_message
