404f3876ae9a58cdae5df975bc0f49df0dcefb7f
Brandon Olin <brandon@devblackops.io>
2019-09-22 20:20:09 -0700

    Make Choco scripts pass validation

--- build/nuget/tools/chocolateyInstall.ps1
+++ build/nuget/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$nugetExecutablePath = Join-Path -Path $env:ChocolateyInstall -ChildPath 'bin'
-$packageBatchFileName = Join-Path -Path $nugetExecutablePath -ChildPath 'psake.bat'
+$nugetBinPath       = Join-Path -Path $env:ChocolateyInstall -ChildPath 'bin'
+$packageBatFileName = Join-Path -Path $nugetBinPath -ChildPath 'psake.bat'
 
 $psakeDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 #$path = ($psakeDir | Split-Path | Join-Path -ChildPath  'psake.cmd')
 $path = Join-Path -Path $psakeDir -ChildPath 'psake/psake.cmd'
-Write-Host "Adding $packageBatchFileName and pointing to $path"
+Write-Host "Adding $packageBatFileName and pointing to $path"
 "@echo off
-""$path"" %*" | Out-File $packageBatchFileName -encoding ASCII
+""$path"" %*" | Out-File $packageBatFileName -encoding ASCII
 
 Write-Host "PSake is now ready. You can type 'psake' from any command line at any path. Get started by typing 'psake /?'"
--- build/nuget/tools/chocolateyUninstall.ps1
+++ build/nuget/tools/chocolateyUninstall.ps1
@@ -1,9 +1,9 @@
-$nugetExecutablePath = Join-Path -Path $env:ChocolateyInstall -ChildPath 'bin'
-$packageBatchFileName = Join-Path -Path $nugetExecutablePath -ChildPath 'psake.bat'
+$nugetBinPath       = Join-Path -Path $env:ChocolateyInstall -ChildPath 'bin'
+$packageBatFileName = Join-Path -Path $nugetBinPath -ChildPath 'psake.bat'
 
 # '[p]sake' is the same as 'psake' but $Error is not polluted
 Remove-Module -Name [p]sake -Verbose:$false
 
-Remove-Item -Path $packageBatchFileName -Force -Confirm:$false
+Remove-Item -Path $packageBatFileName -Force -Confirm:$false
 
 Write-Host 'PSake has been uninstalled'
