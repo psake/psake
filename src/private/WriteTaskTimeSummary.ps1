030ba5cfdab8653873b0f002b973525d3f138056
James Tweedie <jamestweedieuk@gmail.com>
2020-06-18 20:32:02 +0100

    Updated all logging to be routed via WriteOutput function

--- src/private/WriteTaskTimeSummary.ps1
+++ src/private/WriteTaskTimeSummary.ps1
@@ -1,16 +1,9 @@
 function WriteTaskTimeSummary($invokePsakeDuration) {
     if ($psake.context.count -gt 0) {
         $currentContext = $psake.context.Peek()
-        if ($currentContext.config.taskNameFormat -is [ScriptBlock]) {
-            & $currentContext.config.taskNameFormat "Build Time Report"
-        } elseif ($currentContext.config.taskNameFormat -ne "Executing {0}") {
-            $currentContext.config.taskNameFormat -f "Build Time Report"
-        }
-        else {
-            "-" * 70
-            "Build Time Report"
-            "-" * 70
-        }
+        WriteOutput ("-" * 70)
+        WriteOutput "Build Time Report"
+        WriteOutput ("-" * 70)
         $list = @()
         while ($currentContext.executedTasks.Count -gt 0) {
             $taskKey = $currentContext.executedTasks.Pop()
@@ -29,6 +22,6 @@ function WriteTaskTimeSummary($invokePsakeDuration) {
             Duration = $invokePsakeDuration.ToString("hh\:mm\:ss\.fff")
         }
         # using "out-string | where-object" to filter out the blank line that format-table prepends
-        $list | format-table -autoSize -property Name,Duration | out-string -stream | where-object { $_ }
+        $list | format-table -autoSize -property Name,Duration | out-string -stream | where-object { $_ } | WriteOutput
     }
 }
